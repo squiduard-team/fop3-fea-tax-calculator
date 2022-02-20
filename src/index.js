e7e800b437c9f1ad9636eadf55253d76dc5bc483
ValGorovyi <valvetigu@gmail.com>
2022-02-20 14:35:44 +0200

    style width npm

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ function component() {
     const element = document.createElement('div');
 
     // Lodash, now imported by this script
-    element.innerHTML = 'Hello world!';
-    element.classList.add('hello');
+    // element.innerHTML = 'Goodbie world!';
+    // element.classList.add('hello');
 
-    return element;
+    // return element;
 }
 
 document.body.appendChild(component());
